b5ed95319d401d571ae2dbbb98be0fc8e3cb95bc
staivanov <40460292+staivanov@users.noreply.github.com>
2026-07-14 11:52:04 +0300

    HashTable.cpp Node.cpp main.cpp were created. HT class with little functionality.

---
+++ DataStructures/HashTable/HashTable.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "../HashTable/Node.cpp"
+
+using std::cout;
+using std::endl;
+
+class HashTable {
+
+    private:
+        static const int SIZE = 7;
+        Node* dataMap[SIZE];
+        
+
+
+    public:
+        void printTable(){
+            decltype(SIZE) index = 0;
+
+            while(index < SIZE){
+                cout << index << ":" << endl;
+                if(dataMap[index]){
+                    Node* temp = dataMap[index];
+                    while(temp){
+                        cout << "   {" << temp->key << " - " << temp->value << "}";
+                        temp = temp->next;
+                    }
+                }
+            }
+        }
+};
---
+++ DataStructures/HashTable/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "../HashTable/HashTable.cpp"
+using namespace std;
+
+int main(){
+
+    HashTable* ht = new HashTable();
+
+
+
+    return 0;
+}
