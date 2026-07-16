6d0e8b44b96c7684bddee9c06ff7a5cfe82ddc3b
staivanov <40460292+staivanov@users.noreply.github.com>
2026-07-16 17:39:45 +0300

    Two new function in Heap were defined - printHeap() and insert(). Demo in main how things work.

--- DataStructures/Heap/Heap.cpp
+++ DataStructures/Heap/Heap.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <vector>
 
+using std::cout;
+using std::endl;
 using std::vector;
-
 
 class Heap {
 
@@ -11,7 +12,33 @@ class Heap {
 
     // Formula for left child is |2*index+1| and right child |2*index+2|.
     // root | left child | right child | child of the left child | child of the right child
+    public:
+        void insert(int value){
+            heap.push_back(value);
+            int current = heap.size() - 1;
 
+            while ((current > 0) && (heap[current] > heap[parent(current)]))
+            {
+                swap(current, parent(current));
+                current = parent(current);
+            }
+        }
+
+    public:
+        void printHeap(){
+            cout << "\n[";
+            
+            for(int index = 0; index < heap.size(); index++){
+                cout << heap[index];
+
+                if(index < heap.size() -1){
+                    cout << ", ";
+                }
+            }
+            cout << "]" << endl;
+        }
+
+    
     int parent(int index){
         return (index - 1) / 2;
     }
@@ -30,5 +57,10 @@ class Heap {
         heap[indexOne] = heap[indexTwo];
         heap[indexTwo] = temp;
     }
+
+
+
+
+
 
 };
