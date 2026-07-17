97d0cf8c0f806a4eb829c8d18f649624ba3feaa8
staivanov <40460292+staivanov@users.noreply.github.com>
2026-07-18 01:21:14 +0300

    Function minValue searching by value in the rood was defined. Function deleteNode is 2/3 to be defined completely.

--- DataStructures/BSTrecursive/BinarySearchTree.cpp
+++ DataStructures/BSTrecursive/BinarySearchTree.cpp
@@ -9,49 +9,87 @@ class BinarySearchTree {
     public:
         BinarySearchTree(){
             root = nullptr;
-    }
-
-
-    Node* insert(Node* currentNode, int value){
-        if(currentNode == nullptr) return new Node(value);
-
-        if(value < currentNode->value){
-            currentNode->left = insert(currentNode->left, value);
-        } else if( value > currentNode->value) {
-            currentNode->right = insert(currentNode->right, value);
         }
 
-        return currentNode;
-    }
+    public:
+        Node* insert(Node* currentNode, int value){
+            if(currentNode == nullptr) return new Node(value);
 
-    void insert(int value){
-        if(root == nullptr){
-            root = new Node(value);
+            if(value < currentNode->value){
+                currentNode->left = insert(currentNode->left, value);
+            } else if( value > currentNode->value) {
+                currentNode->right = insert(currentNode->right, value);
+            }
+
+            return currentNode;
         }
 
-        insert(root, value);
-    }
+        void insert(int value){
+            if(root == nullptr){
+                root = new Node(value);
+            }
 
-
-    bool contains(Node* currentNode, int value){
-
-        bool nodeExist = true,
-         nonExistingNode = currentNode == nullptr;
-
-        if (nonExistingNode)
-            return !nodeExist;
-
-        if(currentNode->value == value) return nodeExist;
-
-        if(value < currentNode->value){
-            return contains(currentNode->left, value);
-        } else {
-            return contains(currentNode->right, value);
+            insert(root, value);
         }
-    }
+
+        bool contains(Node* currentNode, int value){
+
+            bool nodeExist = true,
+            nonExistingNode = currentNode == nullptr;
+
+            if (nonExistingNode)
+                return !nodeExist;
+
+            if(currentNode->value == value) return nodeExist;
+
+            if(value < currentNode->value){
+                return contains(currentNode->left, value);
+            } else {
+                return contains(currentNode->right, value);
+            }
+        }
+
+        bool contains(int value){
+            return contains(root, value);
+        }
 
 
-    bool contains(int value){
-        return contains(root, value);
-    }
+        Node* deleteNode(Node* currentNode, int value){
+
+            if(value < currentNode->value){
+                currentNode->left = deleteNode(currentNode->left, value);
+            } else { 
+                if(currentNode->left == nullptr && currentNode->right == nullptr){
+                    delete(currentNode);
+
+                    return nullptr;
+                } else if(currentNode->left == nullptr){
+
+                    Node* temp = currentNode->right;
+                    delete(currentNode);
+                    return temp;
+                } else if(currentNode->right == nullptr){
+
+                    Node *temp = currentNode->left;
+                    delete(currentNode);
+                    return temp;
+                } else {
+                     
+                }
+            }
+
+            return currentNode;
+        }
+
+        void deleteNode(int value){
+            root = deleteNode(root, value);
+        }
+
+        int minValue(Node* currentNode){
+            while(currentNode->left != nullptr){
+                currentNode = currentNode->left;
+            }
+
+            return currentNode->value;
+        }
 };
