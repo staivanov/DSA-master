2a13b52e2f09390f0e946e4cb9b2a61ed40b011b
staivanov <40460292+staivanov@users.noreply.github.com>
2026-07-20 16:52:02 +0300

    Selection sort function was declared and defined plus demo in main method.

--- algorithms/basic_sort_algorithms/selection_sort/main.cpp
+++ algorithms/basic_sort_algorithms/selection_sort/main.cpp
@@ -2,9 +2,52 @@
 
 using namespace std;
 
+void selectionSort(int numbers[], unsigned size);
+
+
 int main(){
 
+    constexpr unsigned sz = 8;
+    int numbers[sz] = {9, 15, 3, 55, 4, 100, 28, 100};
+
+    selectionSort(numbers, sz);
+    signed index = 0,
+            lastIndex = sz - 1;
+
+    while(index < sz){
+        if (index == lastIndex) {
+            cout << numbers[index] << endl;
+            break;
+        }
+
+        cout << numbers[index] << ", ";
+        index++;
+    }
 
 
     return 0;
+}
+
+void selectionSort(int numbers[], unsigned size){
+
+    int index, 
+            innerIndex,
+            numbersLen = size;
+
+    for(index = 0; index < numbersLen; index++){
+        int minIndex = index;
+
+        for(innerIndex = index + 1; innerIndex < numbersLen; innerIndex++){
+
+            if(numbers[innerIndex] < numbers[minIndex]){
+                minIndex = innerIndex;
+            }
+        }
+
+        if( index != minIndex){
+            int temp = numbers[index];
+            numbers[index] = numbers[minIndex];
+            numbers[minIndex] = temp;
+        }
+    }    
 }
