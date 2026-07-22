a6580ef9c9ac52235cdc47b76d2da832123c2146
staivanov <40460292+staivanov@users.noreply.github.com>
2026-07-22 23:42:23 +0300

    A new function for traversing in Breadth First Search aka BFS was succsessfully implemented.

--- algorithms/BFS/main.cpp
+++ algorithms/BFS/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include "../BFS/binary_search_tree.cpp"
+
+using namespace std;
 
 int main(){
 
+   BinarySearchTree* bst = new BinarySearchTree();
+   cout << bst->root << endl;
+    bst->insert(50);
+    bst->insert(21);
+    bst->insert(62);
+    bst->BFS();
+   
 
-
-    return 0;
+   return 0;
 }
