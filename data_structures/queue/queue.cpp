ead9a4d5c9c16fd3489bb3ae5a034d86dbccd7e1
staivanov <40460292+staivanov@users.noreply.github.com>
2026-07-28 00:46:03 +0300

    A new ctr was added for deep copy assignment operator alongside with this code is fully documented.

--- data_structures/queue/queue.cpp
+++ data_structures/queue/queue.cpp
@@ -11,13 +11,53 @@ class Queue {
         int length = 0;
 
     public:
-        Queue(int value){
+        /// @brief Create an empty Queue.
+        Queue() = default;
+
+        /// @brief Create a Queue with initial value of `int` type.
+        /// @param `int` type
+        Queue(int value)
+        {
             Node* newNode = new Node(value);
             first = newNode;
             last = newNode;
             length = 1;
         }
 
+        /// @brief Deep Copy copy constructor. 
+        /// @param `const Queue by reference` 
+        Queue(const Queue& other){
+            copyFrom(other);
+        }
+        // Deep Copy Assignment operator
+        Queue& operator=(const Queue& other){
+            // Self-assignment check
+            if(this == &other){
+                return *this;
+            }
+            // Free existing memory before copying.
+            clear();
+            // Copy elements from source queue.
+            copyFrom(other);
+
+            return *this;
+        }
+
+        /// @brief Destructor who prevents memory leaks.
+        ~Queue(){
+            clear();
+        }
+        /// @brief Clear all dynamically allocated nodes.
+        void clear()
+        {
+
+            while(!isQueueEmpty()){
+                dequeue();
+            }
+        }
+
+    /// @brief Add value at the rear (back) end.
+    /// @param `int` type 
     void enqueue(int value){
         Node* newNode = new Node(value);
 
@@ -33,6 +73,8 @@ class Queue {
         length++;
     }
     
+    /// @brief Return and remove value from the front of a Queue.
+    /// @return `int` type
     int dequeue(){
 
         if(isQueueEmpty()) return INT_MIN;
@@ -55,30 +97,47 @@ class Queue {
         return dequeueValue;
     }
 
+    /// @brief Print every item from the Queue in sequential order.
     void printQueue(){
         Node* temp = first;
 
         while(temp){
-            cout << temp->value << endl;
+            cout << temp->value << " ";
             temp = temp->next; 
         }
-    }
 
+        cout << endl;
+    }
+    /// @brief Print element from the front of a Queue.
     void getFirst(){
+        if(isQueueEmpty()) return;
         cout << first->value << endl;
     }
-
+    /// @brief Print element from the rear (back) of a Queue.
     void getLast(){
+        if(isQueueEmpty()) return;
         cout << last->value << endl;
     }
 
-
+    /// @brief Get actual length on the Queue.
     void getLength(){
         cout << length << endl;
     }
-
+    
     private:
+        /// @brief Determines is Queue empty or not.
+        /// @return `true` or `false`
         bool isQueueEmpty(){
             return length == 0;
+        }
+        /// @brief Helper method that clone nodes from another queue.
+        /// @param `const Queue by reference` 
+        void copyFrom(const Queue& other){
+            Node *current = other.first;
+
+            while(current != nullptr){
+                enqueue(current->value);
+                current = current->next;
+            }
         }
 };
