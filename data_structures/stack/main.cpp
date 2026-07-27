91ae55ff7c28d340b0759fc1002f9f5970be0426
staivanov <40460292+staivanov@users.noreply.github.com>
2026-07-28 00:55:18 +0300

    Code indentation was fixed  in main.cpp

--- data_structures/stack/main.cpp
+++ data_structures/stack/main.cpp
@@ -8,10 +8,12 @@ int main(){
     Stack* stack = new Stack(5);
     stack->push(15);
     stack->push(55);
-   
+    
     stack->printStack();
+
     string tkn(15, '*');
     cout << tkn << endl;
+
     cout << "Popped value from the stack is : " << stack->pop() << endl;
 
     return 0;
