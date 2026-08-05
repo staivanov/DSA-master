fa753e857586219e602631c580967b109fa98a95
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2026-08-05 03:21:10 +0300

    Fixing some major logical errors.

--- data_structures/bst_recursive/binary_search_tree_rec.cpp
+++ data_structures/bst_recursive/binary_search_tree_rec.cpp
@@ -1,108 +1,83 @@
 #include <iostream>
 #include "../bst_recursive/node.cpp"
 
-
 class BinarySearchTree {
-    public:
-        Node* root;
+public:
+    Node* root;
 
-        
-    public:
-        BinarySearchTree(){
-            root = nullptr;
+    BinarySearchTree() {
+        root = nullptr;
+    }
+
+    Node* insert(Node* currentNode, int value) {
+        if (currentNode == nullptr) return new Node(value);
+
+        if (value < currentNode->value) {
+            currentNode->left = insert(currentNode->left, value);
+        } else if (value > currentNode->value) {
+            currentNode->right = insert(currentNode->right, value);
         }
 
-    public:
-        Node* insert(Node* currentNode, int value){
-            if(currentNode == nullptr) return new Node(value);
+        return currentNode;
+    }
 
-            if(value < currentNode->value){
-                currentNode->left = insert(currentNode->left, value);
-            } else if( value > currentNode->value) {
-                currentNode->right = insert(currentNode->right, value);
-            }
+    void insert(int value) {
+        root = insert(root, value);
+    }
 
-            return currentNode;
+    bool contains(Node* currentNode, int value) {
+        if (currentNode == nullptr) return false;
+        if (currentNode->value == value) return true;
+
+        if (value < currentNode->value) {
+            return contains(currentNode->left, value);
+        } else {
+            return contains(currentNode->right, value);
         }
+    }
 
+    bool contains(int value) {
+        return contains(root, value);
+    }
 
-        void insert(int value){
-            if(root == nullptr){
-                root = new Node(value);
-            }
+    Node* deleteNode(Node* currentNode, int value) {
+        if (currentNode == nullptr) return nullptr;
 
-            insert(root, value);
-        }
-
-
-        bool contains(Node* currentNode, int value){
-
-            bool nodeExist = true,
-            nonExistingNode = currentNode == nullptr;
-
-            if (nonExistingNode)
-                return !nodeExist;
-
-            if(currentNode->value == value) return nodeExist;
-
-            if(value < currentNode->value){
-                return contains(currentNode->left, value);
+        if (value < currentNode->value) {
+            currentNode->left = deleteNode(currentNode->left, value);
+        } else if (value > currentNode->value) {
+            currentNode->right = deleteNode(currentNode->right, value);
+        } else { // Found node to delete
+            if (currentNode->left == nullptr && currentNode->right == nullptr) {
+                delete currentNode;
+                return nullptr;
+            } else if (currentNode->left == nullptr) {
+                Node* temp = currentNode->right;
+                delete currentNode;
+                return temp;
+            } else if (currentNode->right == nullptr) {
+                Node* temp = currentNode->left;
+                delete currentNode;
+                return temp;
             } else {
-                return contains(currentNode->right, value);
+                int subTreeMin = minValue(currentNode->right);
+                currentNode->value = subTreeMin;
+                currentNode->right = deleteNode(currentNode->right, subTreeMin);
             }
         }
 
+        return currentNode;
+    }
 
-        bool contains(int value){
-            return contains(root, value);
+    void deleteNode(int value) {
+        root = deleteNode(root, value);
+    }
+
+    int minValue(Node* currentNode) {
+        if (currentNode == nullptr) return -1;
+        while (currentNode->left != nullptr) {
+            currentNode = currentNode->left;
         }
-
-        //Delete a leaf Node, Node with left or right child and two children.
-        Node* deleteNode(Node* currentNode, int value){
-
-            if(value < currentNode->value){
-                currentNode->left = deleteNode(currentNode->left, value);
-            } else { 
-                if(currentNode->left == nullptr && currentNode->right == nullptr){
-                    delete(currentNode);
-
-                    return nullptr;
-                } else if(currentNode->left == nullptr){
-
-                    Node* temp = currentNode->right;
-                    delete(currentNode);
-
-                    return temp;
-                } else if(currentNode->right == nullptr){
-
-                    Node *temp = currentNode->left;
-                    delete(currentNode);
-
-                    return temp;
-                } else {
-
-                     int subTreeMin = minValue(currentNode->right);
-                     currentNode->value = subTreeMin;
-                     currentNode->right = deleteNode(currentNode->right, subTreeMin); 
-                }
-            }
-
-            return currentNode;
-        }
-
-
-        void deleteNode(int value)
-        {
-            root = deleteNode(root, value);
-        }
-
-
-        int minValue(Node* currentNode){
-
-            while(currentNode->left != nullptr){
-                currentNode = currentNode->left;
-            }
-
-            return currentNode->value;
-        }
+        return currentNode->value;
+    }
 };
