0669d0d528709aefcfc5d4aac86fb6925c8d46ab
Stanislav Ivanov <40460292+staivanov@users.noreply.github.com>
2026-08-11 22:47:06 +0300

    Code was fully documented.
    Added comments to clarify bubble sort algorithm steps.

--- algorithms/basic_sort_algorithms/bubble_sort/main.cpp
+++ algorithms/basic_sort_algorithms/bubble_sort/main.cpp
@@ -16,18 +16,19 @@ int main(){
 
     return 0;
 }
-
+//Bubble sort is a simple sorting algorithm that works by repeatedly comparing neighboring elements and swapping them if they're in the wrong order.
 void bubbleSort(int numbers[], size_t size){
-
+    // Guard again 1 or zero numbers in the array.
     if(size < 2) return;
     
     size_t lastIndex,
         innerIndex, 
         firstIndex = 0;
-
+    // With every iteration from here, the biggest number from the unsorted numbers is found and placed in its place on the right side.
     for(size_t lastIndex = size - 1; lastIndex > firstIndex; lastIndex--){
+        // Every iteration finds the biggest number and passes it to the right.
         for(innerIndex = 0; innerIndex < lastIndex; innerIndex++){
-
+            // Swap bigger with smaller number in asc order.
             if(numbers[innerIndex] > numbers[innerIndex + 1]){
                 
                 int temp = numbers[innerIndex];
